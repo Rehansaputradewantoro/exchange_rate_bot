d1e3f39322ad02523f4201aa6e01bf5016dbefee
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2024-02-10 01:57:19 +0700

    Update handlers.py

--- utils/handlers.py
+++ utils/handlers.py
@@ -1,4 +1,4 @@
-"""Обработчики параметров."""
+"""Penangan parameter."""
 import os
 from http import HTTPStatus
 
@@ -14,16 +14,16 @@ logger = log_configured.getLogger(__name__)
 
 
 def get_token(key: str) -> str:
-    """Проверяем наличие токена."""
+    """Memeriksa keberadaan token."""
     token: str = os.getenv(key)
     if token is not None:
         return token
-    logger.error('Ошибка, нет токена')
-    raise APIException('Не передан токен для доступа к боту.')
+    logger.error('Kesalahan, tidak ada tanda')
+    raise APIException('Token untuk akses ke bot tidak ditransfer.')
 
 
 async def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
-    """Удалить подписку, если она уже существовала."""
+    """Hapus langganan jika sudah ada."""
     current_jobs = context.job_queue.get_jobs_by_name(name)  # type: ignore
     if not current_jobs:
         return False
@@ -33,19 +33,19 @@ async def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) ->
 
 
 async def send_subscription(context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Отправляем уведомление по подписанным валютам."""
+    """Kami mengirimkan pemberitahuan untuk mata uang berlangganan."""
     job = context.job
     resp = make_request().json()
-    message: str = f'Прошло {job.data[0]} секунд.'  # type: ignore
+    message: str = f'Lulus {job.data[0]} detik.'  # type: ignore
     for currency in job.data[1]:  # type: ignore
         message += f'\n{resp["Valute"][currency]["CharCode"]} = {resp["Valute"][currency]["Value"]:.3f}'
     await context.bot.send_message(str(job.chat_id), text=message)  # type: ignore
 
 
 def make_request(url: str = API_URL) -> Response:
-    """Получение ответа от АПИ валют."""
+    """Menerima respons dari API Mata Uang."""
     resp = requests.get(url)
     if resp.status_code != HTTPStatus.OK:
-        logger.error(f'Ошибочный ответ от сервиса курса валют: {resp.status_code}')
-        raise ServiceException(f'Ошибка ответа от {API_URL}: {resp.text}')
+        logger.error(f'Respon yang salah dari layanan nilai tukar: {resp.status_code}')
+        raise ServiceException(f'Kesalahan respons dari {API_URL}: {resp.text}')
     return resp
