82c6ec898e2272e7cd05cd50ce9d51f690ea76d0
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2024-02-10 01:43:58 +0700

    Update mine.py

--- bot_commands/mine.py
+++ bot_commands/mine.py
@@ -1,4 +1,4 @@
-"""Получение курсов валют к рублю, выбранных пользователем."""
+"""Mendapatkan nilai tukar ke rubel yang dipilih oleh pengguna."""
 from configs import log_configured
 from configs.base import CURRENCIES
 from exceptions import ServiceException
@@ -10,30 +10,30 @@ logger = log_configured.getLogger(__name__)
 
 
 async def sub(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Логика команды /sub."""
+    """untuk menggunakannya ketik /sub."""
     if update.effective_chat is not None:
         message: str = (
-            '<b>Можно подписаться на следующие валюты:</b>\n'
+            '<b>Anda dapat berlangganan mata uang berikut:</b>\n'
             f'{" ".join((cur for cur in CURRENCIES))}\n'
-            'Делается так: /sub 10 USD CNY\n'
-            'Первый параметр - время в секундах, остальные - валюты для подписки\n'
-            'Минимально нужно указать время и хотя бы 1 валюту, иначе не сработает'
+            'Hal ini dilakukan seperti contoh ini: /sub 10 USD CNY\n'
+            'Parameter pertama adalah waktu dalam hitungan detik, sisanya adalah mata uang untuk berlangganan\n'
+             'Minimum Anda perlu menentukan waktu dan setidaknya 1 mata uang, jika tidak, maka tidak akan berfungsi'
         )
 
         if context.args and context.args[0].isnumeric():
             if int(context.args[0]) <= 5:
-                logger.warning('Первый параметр в подписке меньше или равен 5 сек.')
+                logger.warning('Parameter pertama dalam langganan kurang dari atau sama dengan 5 detik.')
                 raise ServiceException(
-                    f'Установите периодичность подписки числом,  большим 5 сек, а не {context.args[0]}',
+                    f'Setel frekuensi berlangganan ke angka lebih dari 5 detik, bukan: {context.args[0]}',
                 )
             subbed_curs: list = []
             for param in context.args[1:]:
                 if param.upper() in CURRENCIES:
                     subbed_curs.append(param.upper())
                 else:
-                    logger.warning(f'Игнорируем недоступную валюту для рассылки: {param.upper()}')
+                    logger.warning(f'Abaikan mata uang yang tidak tersedia untuk pengiriman: {param.upper()}')
             message = (
-                f'Подписан на валюты = <b>{*subbed_curs, }</b> =.\nРассылка раз в {context.args[0]} секунд.'
+                f'Berlangganan mata uang = <b>{*subbed_curs, }</b> =.\nBuletin sekali setiap {context.args[0]} detik.'
             )
 
             await remove_job_if_exists(str(update.effective_chat.id), context)
@@ -50,17 +50,17 @@ async def sub(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             text=message, parse_mode='HTML',
         )
     else:
-        logger.warning('Не получен ID чата при запросе /sub.')
+        logger.warning('untuk memulai berlangganan /sub.')
 
 
 async def unsub(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Отменить подписку."""
+    """Batalkan langganan."""
     if update.effective_chat is not None:
         job_removed = await remove_job_if_exists(str(update.effective_chat.id), context)
-        message = 'Подписка отменена' if job_removed else 'Подписки не было. Отменять нечего.'
+        message = 'Langganan dibatalkan' if job_removed else 'Tidak ada langganan. Tidak ada yang perlu dibatalkan.'
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text=message,
         )
     else:
-        logger.warning('В unsub не передан effective_chat.')
+        logger.warning('Tidak diteruskan ke berhenti berlangganan effective_chat.')
